144d72ab493667e5b94bc00157c4199e9d014afd
shashank7753 <89301898+shashank7753@users.noreply.github.com>
2024-09-04 00:41:07 +0530

    Time: 3 ms (51.28%), Space: 7.9 MB (90.64%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -13,12 +13,7 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        // Note:
-        // We need to perfrom the operations k number of times
-        // Given that the characters in the s will be replaced with corresponsding decimal places.
-        // 123
-        // 1 + 2 + 3
-        // 3 + 2 + 1
+      
         k--;
         int sumVal = 0;
         for(auto &ch : s){
